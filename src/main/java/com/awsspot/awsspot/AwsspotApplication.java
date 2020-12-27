de9c13b8bbce0232b44167487144c49116e214ab
gilad <giladwer@hotmail.com>
2020-12-27 15:12:36 +0200

    first commit

---
+++ src/main/java/com/awsspot/awsspot/AwsspotApplication.java
@@ -0,0 +1,13 @@
+package com.awsspot.awsspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsspotApplication.class, args);
+	}
+
+}
